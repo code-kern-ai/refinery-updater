e222074af86cc92a619f934a950d7a498b964500
JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
2022-09-08 12:09:46 +0200

    Update logic (#7)
    * Adds submodule for model
    
    * Adds update logic and version overview
    
    * Adds flush to print
    
    * Remove dummy functions
    
    * Adds neural search update for v1.2.1.
    
    * Adds has_updates endpoint
    
    * change to dev submodule
    
    * adds option for html response for has_updates
    
    * Adds comments to better explain the upgrade logic function idea
    
    * Resolves PR comments
    
    Co-authored-by: felix0496 <felix.kirsch@kern.ai>

--- app.py
+++ app.py
@@ -1,35 +1,56 @@
-from typing import List, Dict, Optional, Tuple
+from typing import List, Dict
 
-from fastapi import FastAPI
+from fastapi import FastAPI, responses
 from pydantic import BaseModel
+from submodules.model.business_objects import general
 import util
 
 app = FastAPI()
 
 
-class Request(BaseModel):
-    current_version: str
-    target_version: str
+@app.post("/update_to_newest")
+def update_to_newest() -> str:
+    session_token = general.get_ctx_token()
+    if util.update_to_newest():
+        msg = "updated to newest version"
+    else:
+        msg = "nothing to update"
+    general.remove_and_refresh_session(session_token)
+    return msg, 200
 
 
-@app.post("/update_to_version")
-def update_to_version(request: Request) -> int:
-    return util.upgrade_to_version(request.current_version, request.target_version), ""
+@app.get("/version_overview")
+def version_overview() -> List[Dict[str, str]]:
+    session_token = general.get_ctx_token()
+    return_values = util.version_overview()
+    general.remove_and_refresh_session(session_token)
+    return return_values, 200
+
+
+@app.get("/has_updates")
+def has_updates(as_html_response: bool = False) -> bool:
+    session_token = general.get_ctx_token()
+    return_value = util.has_updates()
+    general.remove_and_refresh_session(session_token)
+    if as_html_response:
+        return responses.HTMLResponse(content=str(return_value))
+    return return_value, 200
 
 
 class UpgradeToAWS(BaseModel):
-    only_existing:bool
+    only_existing: bool
     remove_from_minio: bool
     force_overwrite: bool
 
-#special case since this will not be part of the common (OS) Versioning since OS will use minio
+
+# special case since this will not be part of the common (OS) Versioning since OS will use minio
 @app.post("/migrate_minio_to_aws")
-def migrate_minio_to_aws(request:UpgradeToAWS)-> int:
-    from upgrade_logic.upgrade_from_minio_to_aws import upgrade
-    upgrade(request.only_existing,request.remove_from_minio,request.force_overwrite)
+def migrate_minio_to_aws(request: UpgradeToAWS) -> int:
+    from upgrade_logic.pre_redesign.upgrade_from_minio_to_aws import upgrade
+
+    upgrade(request.only_existing, request.remove_from_minio, request.force_overwrite)
 
 
 @app.post("/helper")
 def helper() -> int:
-    from submodules.s3 import controller as s3
-    s3.empty_storage(True,True) 
+    pass
---
+++ upgrade_logic/business_objects/neural_search.py
@@ -0,0 +1,49 @@
+
+import os
+import requests
+
+from submodules.model.business_objects import embedding
+
+
+NEURAL_SEARCH = os.getenv("NEURAL_SEARCH")
+
+
+def neural_search_1_2_1()->bool:
+    print("upgrade qdrant version, recreate collections",flush=True)
+    success = __recreate_qdrant_collections()
+    if success:
+        print("Recreated all collections.",flush=True)
+    else:
+        print("Recreating collections failed.",flush=True)
+    return success
+
+
+def __recreate_qdrant_collections() -> bool:
+
+    success = True
+
+    # recreate collections
+    embedding_items = embedding.get_finished_attribute_embeddings()
+
+    url_recreate = f"{NEURAL_SEARCH}/recreate_collection"
+    for project_id, embedding_id in embedding_items:
+        params = {
+            "project_id": str(project_id),
+            "embedding_id": str(embedding_id),
+        }
+        response = requests.post(url=url_recreate, params=params)
+
+        if response.status_code != 200:
+            print(response.content)
+            success = False
+
+    # create missing collections
+    # to be sure that an collection exists for every embedding
+    url_missing_collections = f"{NEURAL_SEARCH}/create_missing_collections"
+    response = requests.put(url=url_missing_collections)
+
+    if response.status_code != 200 and response.status_code != 412:
+        print(response.content,flush=True)
+        success = False
+
+    return success
