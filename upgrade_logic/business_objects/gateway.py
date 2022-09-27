c77f1d931e0413747bc7974f4ff46454a18c7077
Felix Kirsch <40427105+FelixKirsch@users.noreply.github.com>
2022-09-27 15:23:00 +0200

    Release v1.3.0 (#13)

---
+++ upgrade_logic/business_objects/gateway.py
@@ -0,0 +1,69 @@
+import re
+from submodules.model.business_objects import attribute, embedding, user, general
+
+
+def gateway_1_3_0() -> bool:
+    __gateway_1_3_0_add_engineer()
+    __gateway_1_3_0_add_attribute_for_embedding()
+    __gateway_1_3_0_add_attribute_default_state()
+    return True
+
+
+def __gateway_1_3_0_add_engineer() -> bool:
+    print("add ENGINEER role to existing users", flush=True)
+    users = user.get_all()
+    something_changed = False
+    for u in users:
+        if not u.role:
+            u.role = "ENGINEER"
+            something_changed = True
+    if something_changed:
+        general.commit()
+        print("Added role.", flush=True)
+    else:
+        print("Nothing changed", flush=True)
+    return something_changed
+
+
+def __gateway_1_3_0_add_attribute_for_embedding() -> bool:
+    print("add attribute id to existing embeddings", flush=True)
+    embedding_added = False
+    embedding_items = embedding.get_all_embeddings()
+    for i, embedding_item in enumerate(embedding_items):
+        if embedding_item.attribute_id is None:
+            attribute_name = __get_attribute_name_from_embedding_name(
+                embedding_item.name
+            )
+            attribute_item = attribute.get_by_name(
+                embedding_item.project_id, attribute_name
+            )
+            embedding_item.attribute_id = attribute_item.id
+            embedding_added = True
+    if embedding_added:
+        general.commit()
+        print("Added attribute id.", flush=True)
+    else:
+        print("No attribute id added.", flush=True)
+
+    return embedding_added
+
+
+def __get_attribute_name_from_embedding_name(embedding_name: str) -> str:
+    regex = "^(.+)-(?:classification|extraction).*"
+    return re.match(regex, embedding_name).group(1)
+
+
+def __gateway_1_3_0_add_attribute_default_state() -> bool:
+    print("add UPLOADED state to existing attributes", flush=True)
+    attributes = attribute.get_all(None, None)
+    something_changed = False
+    for a in attributes:
+        if not a.state:
+            a.state = "UPLOADED"
+            something_changed = True
+    if something_changed:
+        general.commit()
+        print("Added state.", flush=True)
+    else:
+        print("Nothing changed", flush=True)
+    return something_changed
--- upgrade_logic/business_objects/general.py
+++ upgrade_logic/business_objects/general.py
@@ -1,3 +1,4 @@
 # function name tamplate: <service_name>_<version_with_underscore>
 # example: update logic for service AUTHORIZER for version 1.2.2 would be def authorizer_1_2_2()->bool:
 # should always return True if update logic was successful
+# private functions can be used as helper functions, since they do not start with the service name
